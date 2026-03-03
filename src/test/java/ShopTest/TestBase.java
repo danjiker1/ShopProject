a4d40fcadcb7c60abe8ca97fd0425c39095f1bbb
danjiker1 <daniil.egorov2003@mail.ru>
2026-03-03 18:50:41 +0300

    Добавил настройки для Jenkins

--- src/test/java/ShopTest/TestBase.java
+++ src/test/java/ShopTest/TestBase.java
@@ -1,5 +1,6 @@
 package ShopTest;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,5 +14,13 @@ public class TestBase {
                 .screenshots(true)
                 .savePageSource(true)
                 .includeSelenideSteps(true));
+
+        // Настройки для Jenkins (headless режим)
+        Configuration.headless = true;  // ← ВАЖНО!
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+
+        System.out.println("✅ TestBase настроен для Jenkins (headless mode)");
     }
 }
